355110e6bf7ceb524b88b39d21628446a77f37c9
aleksandrkosanin <kosyanin@yandex.ru>
2024-02-05 22:12:54 +0300

    создали основу

---
+++ src/main/java/kosyaninchuyko/tgscalping/TgScalpingApplication.java
@@ -0,0 +1,13 @@
+package kosyaninchuyko.tgscalping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgScalpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgScalpingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kosyaninchuyko/tgscalping/TgScalpingApplicationTests.java
@@ -0,0 +1,13 @@
+package kosyaninchuyko.tgscalping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgScalpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
